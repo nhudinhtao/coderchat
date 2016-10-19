75a9a483c317c18c98439b7db3e8d8acdf7920bc
Tao Nhu <%%GITEMAIL%%>
2016-10-19 20:17:57 +0700

    implement login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if @user = User.find_by_email(params[:email])
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        flash[:success] = 'Login successfully.'
+        redirect_to root_path
+      else
+        flash.now[:error] = 'Incorrect password.'
+      end
+    else
+      flash.now[:error] = 'User not found.'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Logout successfully.'
+    redirect_to login_path
+  end
+end
